93ae824fc62c56f027987e806718a5d04bba6774
Fabricio <105023101+GomezFabricio@users.noreply.github.com>
2023-08-24 22:33:45 -0300

    Add files via upload

---
+++ classDNI.py
@@ -0,0 +1,6 @@
+# Constructor de la clase DNI que recibe un número de identificación
+# fiscal (DNI) y lo asigna a la propiedad "numero"
+
+class DNI:
+    def __init__(self, numero):
+        self.numero = numero
